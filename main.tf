b28feb3a1ec0a7f1f56b24a4466be28b9ebb8aad
shadabcmsdevops-wq <shadabcms.devops@gmail.com>
2026-01-22 04:23:26 +0530

    Edited pipeline with vm code
    Added resources for random string, virtual network, subnet, network security group, public IP, network interface, and a Linux virtual machine.

--- main.tf
+++ main.tf
@@ -1,8 +1,13 @@
+# main.tf
 terraform {
   required_providers {
     azurerm = {
-      source  = "registry.terraform.io/hashicorp/azurerm"
-      version = "~> 3.0"
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
     }
   }
 }
@@ -11,8 +16,118 @@ provider "azurerm" {
   features {}
 }
 
+# Random string for unique names
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+  upper   = false
+}
+
+# Resource Group (already works)
 resource "azurerm_resource_group" "rg" {
   name     = "rg-jenkins-demo"
   location = "East US"
 }
 
+# Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-jenkins-${random_string.suffix.result}"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-jenkins"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Network Security Group (allow SSH/RDP)
+resource "azurerm_network_security_group" "nsg" {
+  name                = "nsg-jenkins-${random_string.suffix.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# Public IP
+resource "azurerm_public_ip" "publicip" {
+  name                = "pip-jenkins-${random_string.suffix.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+}
+
+# Network Interface
+resource "azurerm_network_interface" "nic" {
+  name                = "nic-jenkins-${random_string.suffix.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.publicip.id
+  }
+}
+
+# Network Interface Security Group Association
+resource "azurerm_network_interface_security_group_association" "nsga" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+# Virtual Machine (Ubuntu 22.04)
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "vm-jenkins-${random_string.suffix.result}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B2s"
+  admin_username      = "azureuser"
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAB...your-public-key-here"
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+
+  tags = {
+    Environment = "Demo"
+    ManagedBy   = "Terraform"
+  }
+}
+
+# Output VM details
+output "vm_public_ip" {
+  value = azurerm_public_ip.publicip.ip_address
+}
