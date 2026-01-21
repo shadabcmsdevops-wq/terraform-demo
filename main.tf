a2808ce71fdcd7329128cd2e444daa296b5fda4d
shadabcmsdevops-wq <shadabcms.devops@gmail.com>
2026-01-22 04:46:38 +0530

    gave ip address as static.

--- main.tf
+++ main.tf
@@ -69,7 +69,7 @@ resource "azurerm_public_ip" "publicip" {
   name                = "pip-jenkins-${random_string.suffix.result}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
   sku                 = "Standard"    # ← CHANGE THIS
   sku_tier            = "Regional"    # Keep this
 }
