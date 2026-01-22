e0d30b1f8b02ac1c815b9657cf39fca7aaf9ddeb
shadabcmsdevops-wq <shadabcms.devops@gmail.com>
2026-01-22 06:01:56 +0530

    size changed again.

--- main.tf
+++ main.tf
@@ -99,7 +99,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   name                = "vm-jenkins-${random_string.suffix.result}"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  size                = "Standard_DS1_v2"
+  size                = "Standard_B2s"
   admin_username      = "azureuser"
 admin_password      = "P@ssw0rd123!AzureVM456"  # ← ADD THIS
   disable_password_authentication = false        # ← ADD THIS
